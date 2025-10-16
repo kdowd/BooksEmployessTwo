0767a62a702ab1a8e1e7616c93196f09961c6a80
kdowd <kevin.dowd@gmail.com>
2025-10-16 22:48:35 +1300

    new window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using BooksEmployess.Pages;
+using BooksEmployess.Windows;
 namespace BooksEmployess
 {
 
@@ -42,14 +44,44 @@ namespace BooksEmployess
 
 
         }
+        private void OnAboutClick(object sender, RoutedEventArgs e)
 
+        {
+
+
+
+
+        }
+
+        private void openBooksDetailWindow(Dictionary<string, string> BookData)
+        {
+            Window win2 = new ShowBook(BookData);
+            win2.Owner = Application.Current.MainWindow;
+            win2.ShowDialog();
+        }
 
         private void MyDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
-            if (MyDataGrid.SelectedItem is BookDTO selectedBook)
+            DataGrid clickedElement = (DataGrid)sender;
+            BookDTO selectedBook = (BookDTO)clickedElement.SelectedItem;
+
+
+
+            if (clickedElement != null && selectedBook != null)
 
             {
+                Dictionary<string, string> BookData = new Dictionary<string, string>();
+                BookData.Add("title", $"Title:\n{selectedBook.Title}");
+                BookData.Add("author", $"Author:\n{selectedBook.Author}");
+                BookData.Add("description", $"Description:\n{selectedBook.Description}");
+                BookData.Add("pages", $"Pages:\n{selectedBook.Pages}\n");
+                BookData.Add("year", $"Year of Publication:\n{selectedBook.YearOfPublication}\n");
+                BookData.Add("imagepath", selectedBook.ImageLink);
+
+
+
+
                 string message = $"Title: {selectedBook.Title}\n" +
 
                                  $"Author: {selectedBook.Author}\n" +
@@ -60,8 +92,8 @@ namespace BooksEmployess
 
                                  $"Year of Publication: {selectedBook.YearOfPublication}";
 
-
-                MessageBox.Show(message, "Book Details");
+                openBooksDetailWindow(BookData);
+                //MessageBox.Show(message, "Book Details");
 
             }
 
---
+++ Windows/ShowBook.xaml.cs
@@ -0,0 +1,79 @@
+﻿using BooksEmployess.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BooksEmployess.Windows
+{
+    /// <summary>
+    /// Interaction logic for ShowBook.xaml
+    /// </summary>
+    public partial class ShowBook : Window
+    {
+        public ShowBook(Dictionary<string, string> BookData)
+        {
+            init();
+            this.Title = "Book Details";
+            title.Text = BookData["title"];
+            description.Text = BookData["description"];
+            author.Text = BookData["author"];
+            pages.Text = BookData["pages"];
+            year.Text = BookData["year"];
+
+
+            //poster.Source = new BitmapImage(new Uri(BookData["imagepath"]));
+            //poster.Width = 300;
+            //poster.Height = 300;
+            //poster.SourceUpdated
+
+
+            //Image myImage = new Image();
+            //BitmapImage bitmap = new BitmapImage();
+            //bitmap.BeginInit();
+            //bitmap.UriSource = new Uri("https://placehold.co/300x400?text=Alice's+Adventures+in+Wonderland", UriKind.Absolute);
+            //bitmap.EndInit();
+            //poster.Source = bitmap;
+            //poster.Height = bitmap.Height;
+            //poster.Width = bitmap.Width;
+
+
+
+        }
+
+        // Replace the line causing the error with the following:
+
+
+
+        private void init()
+        {
+            InitializeComponent();
+            Owner = Application.Current.MainWindow;
+
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var image = new Image();
+            var fullFilePath = @"https://placehold.co/300x400?text=Alice's+Adventures+in+Wonderland";
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(fullFilePath, UriKind.Absolute);
+            bitmap.EndInit();
+
+            // image.Source = bitmap;
+            poster.Source = bitmap;
+            // MyStackPanel.Children.Add(image);
+        }
+    }
+}
